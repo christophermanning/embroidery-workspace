df4e8f3c1e4f587a3ebd65a2372147911b785fae
Christopher Manning <289331+christophermanning@users.noreply.github.com>
2024-07-09 14:50:21 -0500

    turtle#center: only modify 0 and 1 index of self.points

--- patterns/turtle.py
+++ patterns/turtle.py
@@ -19,14 +19,23 @@ class Turtle(TNavigator):
         self.teleport(*self.points[-1])
 
     def center(self, x, y):
-        ls = LineString(self.points)
+        if len(self.points) == 0:
+            return None
+
+        ls = LineString([p[0:2] for p in self.points])
 
         midpoint = [
             (ls.bounds[2] + ls.bounds[0]) / 2,
             (ls.bounds[3] + ls.bounds[1]) / 2,
         ]
 
+        offset_x = x - midpoint[0]
+        offset_y = y - midpoint[1]
         ls = affinity.translate(ls, x - midpoint[0], y - midpoint[1])
 
-        self.points = list(ls.coords)
-        self.teleport(*self.points[-1])
+        self.teleport(*ls.coords[-1])
+
+        # restore any additional elements that were in self.points
+        self.points = [c + tuple(self.points[i][2:]) for i, c in enumerate(ls.coords)]
+
+        return (offset_x, offset_y)
