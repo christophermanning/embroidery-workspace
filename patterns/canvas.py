ef86049e63bf2343fbf0bb15cada8bd00321ee9c
Christopher Manning <289331+christophermanning@users.noreply.github.com>
2024-07-16 20:49:48 -0500

    canvas: remove unused functions

--- patterns/canvas.py
+++ patterns/canvas.py
@@ -5,7 +5,7 @@ from patterns.util import distance
 from patterns import CanvasPattern
 
 
-# Canvas represends the embroidery suface and the dimensions are machine specific
+# Canvas represents the embroidery suface and the dimensions are machine specific
 # 4 x 4 size hoop with a width and height of 1000 supports a maximum of 30,000 stitches.
 class Canvas:
     # minimimum unit distance between stitches
@@ -31,17 +31,3 @@ class Canvas:
         ]
 
         self.pattern += initial_color
-
-    def rand_point(self):
-        return (
-            random.uniform(0 + self.margin, self.width - self.margin),
-            random.uniform(0 + self.margin, self.height - self.margin),
-        )
-
-    def rand_point_from(self, x, y, min_dist, max_dist):
-        for i in range(0, 100):
-            rx, ry = self.rand_point()
-
-            d = distance((x, y), (rx, ry))
-            if d > min_dist and d < max_dist:
-                return (rx, ry)
